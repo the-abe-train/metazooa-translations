010369cf283d4e300670de7b27e9f00bf551f130
Abe Train <47761569+the-abe-train@users.noreply.github.com>
2025-01-21 10:25:21 -0500

    feat: Add Turkish language support and update Czech translations

--- add_animals.py
+++ add_animals.py
@@ -1,35 +1,40 @@
 import json
 
-with open("data/animals_uk.json", "r") as f:
-    new_animals = json.load(f)
+new_lang = "tr"
+kingdom = "animals"
 
-with open("Species Lists/animals.json", "r") as f:
-    old_animals = json.load(f)
+with open(f"data/{kingdom}_{new_lang}.json", "r") as f:
+    new_species = json.load(f)
 
-nodes = [animal["node"] for animal in old_animals]
-new_nodes = [animal["node"] for animal in new_animals]
+with open(f"Species Lists/{kingdom}.json", "r") as f:
+    old_species = json.load(f)
+
+nodes = [species["nodeId"] for species in old_species]
+new_nodes = [species["nodeId"] for species in new_species]
 
 for new_node in new_nodes:
     if new_node not in nodes:
-        animal = new_animals[new_nodes.index(new_node)]
-        print(f"Node {new_node} not found in old animals")
-        print(animal)
+        species = new_species[new_nodes.index(new_node)]
+        print(f"Node {new_node} not found in old species")
+        print(species)
 
 
 output = []
 
-for animal in old_animals:
-    node = animal["node"]
+for species in old_species:
+    node = species["nodeId"]
     if node in new_nodes:
-        new_animal = new_animals[new_nodes.index(node)]
-        new_name = new_animal["name_de"]
-        animal["name_de"] = new_name
-        output.append(animal)
+        print(node)
+        new_animal = new_species[new_nodes.index(node)]
+        field = f"name_{new_lang}"
+        new_name = new_animal[field]
+        species[field] = new_name
+        output.append(species)
     else:
         raise Exception(f"Node {node} not found in new animals")
 
 
-with open("data/animals_added.json", "w") as f:
+with open(f"data/{kingdom}_added.json", "w") as f:
     json.dump(output, f, indent=4, ensure_ascii=False)
 
 print("Done")
